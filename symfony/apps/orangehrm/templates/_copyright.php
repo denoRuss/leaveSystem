ef95084a749bb017a21e088652f7bb09be5e4017
denoruss <denoruss>
2019-05-14 14:27:49 +0530

    change brand name

--- symfony/apps/orangehrm/templates/_copyright.php
+++ symfony/apps/orangehrm/templates/_copyright.php
@@ -5,9 +5,9 @@ if (@include_once $rootPath."/lib/confs/sysConf.php") {
     $conf = new sysConf();
     $version = $conf->getVersion();
 }
-$prodName = 'OrangeHRM';
+$prodName = 'Diakonia';
 $copyrightYear = date('Y');
-
+$version = '1.0'
 ?>
-<?php echo $prodName . ' ' . $version;?><br/>
-&copy; 2005 - <?php echo $copyrightYear;?> <a href="http://www.orangehrm.com" target="_blank">OrangeHRM, Inc</a>. All rights reserved.
+<?php echo $prodName . ' ' . $version;?> - &copy; <?php echo $copyrightYear;?>
+<!--&copy; $copyrightYear - --><?php //echo $copyrightYear;?><!-- <a href="http://www.orangehrm.com" target="_blank">OrangeHRM, Inc</a>. All rights reserved.-->
