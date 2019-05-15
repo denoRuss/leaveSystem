e380c1a30edcf468288b10cb820b55ec2d030d05
denoruss <denoruss>
2019-05-15 16:51:04 +0530

    basecode change (remove subunit filter and column from employee list screen)

--- symfony/plugins/orangehrmPimPlugin/modules/pim/templates/viewEmployeeListSuccess.php
+++ symfony/plugins/orangehrmPimPlugin/modules/pim/templates/viewEmployeeListSuccess.php
@@ -58,7 +58,8 @@
 <script type="text/javascript">
 
     $(document).ready(function() {
-        
+
+        $("#empsearch_sub_unit").parent().hide();
         var supervisors = <?php echo str_replace('&#039;', "'", $form->getSupervisorListAsJson()) ?>;
 
         $('#btnDelete').attr('disabled', 'disabled');
